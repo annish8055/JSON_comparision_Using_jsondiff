46bbab10a76900e58d863b185b6d32301d944ed4
Annish Kumar <ann2000wat@gmail.com>
2019-06-07 12:53:05 +0530

    Add files via upload

---
+++ output/data.js
@@ -0,0 +1,7 @@
+myData = [
+{x1:"./jsonfiles\file1.json --> Mismatched values "},
+{x1:"Key: data , values: {0: { [(0; 'I0H 0M5'); (1; 'Lawrence')]}; 1: { [(0; 'B8C 7P8'); (1; 'Griffin'); (2; 'Angelica'); (3; 'Wallis and Futuna')]}; 89: {other_file_issue_number: [4; 3; 2]}}"},
+{x1:"./jsonfiles\file2.json --> Mismatched values "},
+{x1:"Key: data , values: {0: {other_file_issue_number: [1; 0]}; 1: {other_file_issue_number: [3; 2; 1; 0]}; 89: { [(2; 'Craig'); (3; 'Turkey'); (4; '31126')]}}"},
+
+];
---
+++ output/file.js
@@ -0,0 +1,1 @@
+extradata = {"line1": "Mismatch in files", "line3": "Incomplete match", "line3C": "red", "line2": 50}
